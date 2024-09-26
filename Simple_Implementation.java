7e7d46f6577268a0462ce7e3afb8d8984400b65d
lyllesubesa <subesalylle909@gmail.com>
2024-09-25 16:36:55 -1200

    hehehhe

---
+++ Simple_Implementation.java
@@ -0,0 +1,13 @@
+package simple_implementation;
+
+public class Simple_Implementation {
+    public static void main(String[] args) {
+        // Added a list of array for input
+        String[] colors = {"red", "white", "yellow", "black"};
+        
+        // Added a process & output using S.O.P.
+        System.out.println(colors[1]);
+        System.out.println(colors[0]);
+        System.out.println(colors[2]);
+    }
+}
