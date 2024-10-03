2b2b6bc6c96d8873e6327aed61203034bac537ac
lyllesubesa <subesalylle909@gmail.com>
2024-10-04 00:43:49 +0800

    Added integer linklist

--- main.java
+++ main.java
@@ -1,96 +1,89 @@
-package main;
-
-
- 
+package topic_2_linkedlist_int;
 
 import java.util.Scanner;
 public class Main {
-
-
-    public static void main(String[] args) {
-        
-        
-          ItemManager list = new ItemManager();
-          
-          Scanner lylle = new Scanner(System.in);
-          
-          
-                    
-                   while(true){
-         System.out.println("1. Entrance");
-          System.out.println("2. Insert");
-          System.out.println("3. Display");
-          System.out.println("4. Updated");
-          System.out.println("5. find items");
-          System.out.println("6. Deleted all items");
-           System.out.println("7. Leave");
-       
-           String scan = lylle.nextLine();
-           
-           
-           if( scan.equals("1")) {
-               System.out.println("Maayung pagsulod Amego");
-                String Items = lylle.nextLine();
-                     
     
-           }else if( scan.equals("2")) {
-               System.out.println("Unsa Man Imong Gusto e Insert?");
-                String Items = lylle.nextLine();
-                System.out.println();
-                list.insertItem( Items);
-                     System.out.println();
-                     
-                     
-           } else if( scan.equals("3")){
-                     
-                     list.displayItems();
-                        System.out.println();
-                
-                if( scan.equals("4")) {
-               System.out.println("Basin naa kay gusto e update?");
-            int item = Integer.parseInt(lylle.nextLine());
-            
-                     System.out.println(" Unsa imong gusto ilisan nga item? ");
-                     String repItem = lylle.nextLine();
-                     list.updateItem(item, repItem);
-                      System.out.println();
-                      
-                       } else if( scan.equals("5")){
-                     
-                     System.out.println("Og unsa pod imong gusto e makit an? ");
-                     String item = lylle.nextLine();
-                     list.findItem(item);
-                        System.out.println();
-                     
-                     
-                 
-                 } else if( scan.equals("6")){
-                     
-                    System.out.println("Unsa nga item ang gusto nimo e delete?");
-                    list.displayItems();
-                     int  index = Integer.parseInt(lylle.nextLine()) ;
-                     list.deleteItem(index);
-                     System.out.println();
-                    
-                  
-                        
-                 } else if( scan.equals("7")){
-                     
-                     System.out.println("Matsalam amgeeoo!! ");
-                    break;
-                     
-                
-                 } else {
-                     System.out.println("shesssshhh");
-                     
-                  
-                 }
-                 
-                 
-                 
-                 }
-            
-        
-                   }
+    public static void main(String[] args) {
+
+          LinkedListBylylle list = new LinkedListlylle();
+          
+          Scanner scan = new Scanner(System.in);
+          
+          
+          System.out.println("Welcome to the linked List =)");
+          
+          
+          while(true){
+          
+          System.out.println("What do you want to do in the list ? ");
+          System.out.println("1. Add");
+          System.out.println("2. Delete");
+          System.out.println("3. Move/Swap");
+          System.out.println("4. Display");
+          System.out.println("5. Exit ");
+          
+          String answer = scan.nextLine();
+          
+          
+          
+          //  Adding elements to the list
+          if(answer.equals("1")){
+              for(int i = 0; i <= 0; i++){
+               System.out.println("What elements would you like to add?");
+              int eltoadd = Integer.parseInt(scan.nextLine());
+              list.add(eltoadd);
+              
+              
+              }
+               for (int i = 0; i < 5 ; i++ ){
+              System.out.println("Would you still want to add an element?");
+              String ans = scan.nextLine();
+             
+              if(ans.equals("Yes")){
+                  System.out.println("");
+                  int eltoadd = Integer.parseInt(scan.nextLine());
+              list.add(eltoadd);
+              }else{
+                  break;
+              }
+              }
+              // Delete
+          } else if(answer.equals("2")){
+              
+              System.out.println("What element would you like to delete?");
+               int eltodele = Integer.parseInt(scan.nextLine());
+               list.deleteByValue(eltodele);
+              
+              
+               // Move or Swap
+          }else if(answer.equals("3")){
+              
+              System.out.println("What is the fist element would you like to move/swap?");
+              int eltoMoveorSwap1 = Integer.parseInt(scan.nextLine());
+              System.out.println("What is the second element would you like to move/swap?");
+              int eltoMoveorSwap2 = Integer.parseInt(scan.nextLine());
+              
+              list.moveNodePointer(eltoMoveorSwap2, eltoMoveorSwap2);
+              list.printList();
+          }else if(answer.equals("4")){
+              
+             list.printList();
+             
+          }else if(answer.equals("5")){
+              g
+              System.out.println("Thank you");
+              break;
+               
+              
+              
+          }else{
+              System.out.println("heheheheh");
+              
+          }
+
+
+
     }
+    
+}
 }
